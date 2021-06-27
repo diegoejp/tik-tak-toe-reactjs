a43585e7df0b83f5d8ea2e4b911a75010f5b7813
diegoejp <d.jacobpulgar@gmail.com>
2021-06-27 15:11:36 -0400

    empezando inicio

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import {BrowserRouter,Route} from "react-router-dom"
+
+function Navegador(){
+    return(
+        <>
+        <BrowserRouter>
+            <Route exact path="/app" component={App}/>
+        </BrowserRouter>
+        </>
+    )
+}
 
 
-ReactDOM.render(<App/>,document.querySelector("#root"));
+ReactDOM.render(<Navegador/>,document.querySelector("#root"));
 
 
 
